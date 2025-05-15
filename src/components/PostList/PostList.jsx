a152f24a86f4684aba1bf4941305a283eaee8afd
Veronika Okhten <veronika.oxt12@gmail.com>
2025-05-15 17:36:22 +0300

    add task

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import { PostInfo } from '../PostInfo';
+import { PostInfo } from '../PostInfo/PostInfo';
 import './PostList.scss';
 
 export const PostList = ({ posts }) => (
-  <div className="posts">
+  <div className="PostList">
     {posts.map(post => (
       <PostInfo post={post} key={post.id} />
     ))}
